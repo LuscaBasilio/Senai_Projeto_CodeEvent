1018a23425e63fb9edfac69d389126e2b234bc6e
Lucas Basilio Leite <47511187811@info.local>
2019-08-05 11:30:59 -0300

    Projeto criado

---
+++ Senai_CodeEvent_WebApi/Senai_CodeEvent_Dominio/Entidade/EventoDominio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senai_CodeEvent_Dominio.Entidade
+{
+    public class EventoDominio
+    {
+    }
+}
---
+++ Senai_CodeEvent_WebApi/Senai_CodeEvent_Servico/Repositorios/EventoRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senai_CodeEvent_Servico.Repositorios
+{
+    public class EventoRepositorio
+    {
+    }
+}
